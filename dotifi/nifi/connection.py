de0835ce0e8fc9be787e1723c49af44f49c9dba3
Otto Fowler <ottobackwards@gmail.com>
2020-05-28 22:29:29 -0400

    pylint and nifi connection start

---
+++ dotifi/nifi/connection.py
@@ -0,0 +1,10 @@
+import nipyapi
+from confuse import LazyConfig
+
+
+def configure_nifi_connection(configuration):
+    """
+    Configure the NiFi connection based on the configuration
+    :param configuration: confuse.LazyConfig
+    """
+
