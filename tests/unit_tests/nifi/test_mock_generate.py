7cf720e993c93fa6d21f511f7104d3146292d976
Otto Fowler <ottobackwards@gmail.com>
2020-06-08 13:03:06 -0400

    test for templating pg with modifications for it to work

--- tests/unit_tests/nifi/test_mock_generate.py
+++ tests/unit_tests/nifi/test_mock_generate.py
@@ -23,6 +23,9 @@ PROCESSOR_CONFIG = pathlib.Path(__file__).parent.parent.parent.joinpath(
 GRAPH_TEMPLATE_CONFIG = pathlib.Path(__file__).parent.parent.parent.joinpath(
     "resources/graph_template.yaml"
 )
+PG_TEMPLATE_CONFIG = pathlib.Path(__file__).parent.parent.parent.joinpath(
+    "resources/pg_template.yaml"
+)
 
 
 def _find_subgraph(graph: AGraph, id: str):
@@ -89,7 +92,7 @@ def test_start_pg(pg_start_args):
 @pytest.mark.parametrize("path_arg", [HAPPY_MOCK_DATA])
 def test_rpg_attribute_override(default_args):
     """
-    Test overriding the graph_attr of a specific remote program group
+    Test overriding the graph_attr of a specific remote process group
     using the yaml configuration
     :param default_args the default_args fixture
     """
@@ -107,8 +110,8 @@ def test_rpg_attribute_override(default_args):
     default_subgraph = _find_subgraph(
         base_graph, "65f8c7d5-0172-1000-a916-0e5562295e08"
     )
-    assert rpg_subgraph.graph_attr["color"] == "yellow"
-    assert rpg_subgraph.graph_attr["shape"] == "star"
+    assert rpg_subgraph.graph_attr.get("color") == "yellow"
+    assert rpg_subgraph.graph_attr.get("shape") == "star"
     assert rpg_subgraph.graph_attr.get("shape") != default_subgraph.graph_attr.get(
         "shape"
     )
@@ -123,6 +126,7 @@ def test_processor_attribute_override(default_args):
     Test overriding the attr of a specific processor
     using the yaml configuration
     :param default_args the default_args fixture
+    :param path to the configuration
     """
 
     options = load_configuration(default_args, PROCESSOR_CONFIG.as_posix())
@@ -138,8 +142,8 @@ def test_processor_attribute_override(default_args):
     default_processor_node = _find_node(
         base_graph, "f96be8b1-78b2-42f2-6ba5-2579f4f6c411"
     )
-    assert processor_node.attr["color"] == "yellow"
-    assert processor_node.attr["shape"] == "star"
+    assert processor_node.attr.get("color") == "yellow"
+    assert processor_node.attr.get("shape") == "star"
     assert processor_node.attr.get("shape") != default_processor_node.attr.get("shape")
     assert processor_node.attr.get("color") != default_processor_node.attr.get("color")
 
@@ -147,9 +151,10 @@ def test_processor_attribute_override(default_args):
 @pytest.mark.parametrize("path_arg", [HAPPY_MOCK_DATA])
 def test_graph_template(default_args):
     """
-    Test overriding the graph_attr of a specific remote program group
-    using the yaml configuration
+    Test overriding the root graph configuration
+    using the yaml configuration specified dot file
     :param default_args the default_args fixture
+    :param path to the configuration
     """
 
     options = load_configuration(default_args, GRAPH_TEMPLATE_CONFIG.as_posix())
@@ -167,11 +172,49 @@ def test_graph_template(default_args):
     assert graph.number_of_nodes() == base_graph.number_of_nodes()
     assert sorted(dict(graph.edges())) == sorted(dict(base_graph.edges()))
     assert sorted(graph.nodes()) == sorted(base_graph.nodes())
-    assert graph.graph_attr["label"] == "My Business"
-    assert graph.node_attr["shape"] == "circle"
+    assert graph.graph_attr.get("label") == "My Business"
+    assert graph.node_attr.get("shape") == "circle"
     assert graph.node_attr.get("shape") != base_graph.node_attr.get("shape")
     assert graph.graph_attr.get("label") != base_graph.graph_attr.get("label")
 
-    from dotifi.publishing.publish import publish
 
-    publish(options, graph)
+@pytest.mark.parametrize("path_arg", [HAPPY_MOCK_DATA])
+def test_pg_template(default_args):
+    """
+    Test overriding the graph configuration of a specific process group
+    using the yaml configuration specified dot file
+    :param default_args the default_args fixture
+    :param path to the configuration
+    """
+
+    options = load_configuration(default_args, PG_TEMPLATE_CONFIG.as_posix())
+    options["process_groups"]["351dbb56-0172-1000-21fa-a4af9fc0dbb1"]["template"] = (
+        pathlib.Path(__file__)
+        .parent.parent.parent.joinpath(
+            "resources/"
+            + options["process_groups"]["351dbb56-0172-1000-21fa-a4af9fc0dbb1"][
+                "template"
+            ].get()
+        )
+        .as_posix()
+    )
+    graph = generate_graph(options)
+    base_graph = AGraph(SAMPLE_GRAPH.as_posix())
+    assert graph is not None
+    assert graph.number_of_edges() == base_graph.number_of_edges()
+    assert graph.number_of_nodes() == base_graph.number_of_nodes()
+    assert sorted(dict(graph.edges())) == sorted(dict(base_graph.edges()))
+    assert sorted(graph.nodes()) == sorted(base_graph.nodes())
+    pg_subgraph = _find_subgraph(graph, "351dbb56-0172-1000-21fa-a4af9fc0dbb1")
+    default_subgraph = _find_subgraph(
+        base_graph, "351dbb56-0172-1000-21fa-a4af9fc0dbb1"
+    )
+    assert pg_subgraph.graph_attr.get("label") == "My Business"
+    assert pg_subgraph.node_attr.get("shape") == "circle"
+    assert pg_subgraph.edge_attr.get("style") == "dashed"
+
+    assert pg_subgraph.graph_attr.get("label") != default_subgraph.graph_attr.get(
+        "label"
+    )
+    assert pg_subgraph.node_attr.get("shape") != default_subgraph.node_attr.get("shape")
+    assert pg_subgraph.edge_attr.get("style") != default_subgraph.edge_attr.get("style")
